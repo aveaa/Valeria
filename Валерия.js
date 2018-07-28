61398eda8eacbceb1e4d3338d92f07efadaeb9fe
NordTort <41715818+NorthTort@users.noreply.github.com>
2018-07-28 06:16:25 +0300

    Update Валерия.js

--- Валерия.js
+++ Валерия.js
@@ -39,7 +39,7 @@ if(message.content.startsWith(prefix + 'djalib')) {
         message.delete()
   .then(msg => console.log(`Deleted message from ${msg.author.username}`))
   .catch(console.error); 
-message.channel.send({
+message.channel.send( 'kjk ', {
   embed: {
     thumbnail: {
          url: 'attachment://file.png'
