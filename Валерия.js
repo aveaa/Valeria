5981a62483caba178f1ba9c9a3cdf246450fe6ff
NordTort <41715818+NorthTort@users.noreply.github.com>
2018-07-28 06:52:49 +0300

    Update Валерия.js

--- Валерия.js
+++ Валерия.js
@@ -112,7 +112,103 @@ message.channel.send({
 .then(console.log)
 .catch(console.error);
 }
-	 
+// Великая Орда
+
+if(message.content.startsWith(prefix + 'haggesh')) {
+        message.delete()
+  .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+  .catch(console.error); 
+message.channel.send({
+  embed: {
+    thumbnail: {
+         url: 'attachment://file.png'
+      }
+   },
+   files: [{
+      attachment: 'https://downloader.disk.yandex.ru/preview/a7f8080e76fe9f0ec5c5882c2b90a624a32e07bf61824bfdafdfe0a2d99283a9/5b5c1e66/_B0aXmp4RJTYYcc2mgnKlnr1wJoBYht69x8vdJWA3wkcZN2Y4PmWzJlp67vQpn-BCIiwyrKPEyXSSEF2bWY0HA%3D%3D?uid=0&filename=Haggesh.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&tknv=v2&size=2048x2048',
+      name: 'Haggesh.png'
+   }]
+})
+.then(console.log)
+.catch(console.error);
+}
+
+if(message.content.startsWith(prefix + 'krag')) {
+        message.delete()
+  .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+  .catch(console.error); 
+message.channel.send({
+  embed: {
+    thumbnail: {
+         url: 'attachment://file.png'
+      }
+   },
+   files: [{
+      attachment: 'https://downloader.disk.yandex.ru/preview/250f51a142f44baaacf3e9072d5cf501b1366bd1f678b472f89b465d4d7afa6a/5b5c1eda/_B0aXmp4RJTYYcc2mgnKlsN4ZxawOZTInUxd7HOcouPFBB3R-fcOi_DE5xhrOsnOA4gYqX_7aivV4TIhsGVuEQ%3D%3D?uid=0&filename=Krag.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&tknv=v2&size=2048x2048',
+      name: 'Krag.png'
+   }]
+})
+.then(console.log)
+.catch(console.error);
+}
+
+if(message.content.startsWith(prefix + 'shak')) {
+        message.delete()
+  .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+  .catch(console.error); 
+message.channel.send({
+  embed: {
+    thumbnail: {
+         url: 'attachment://file.png'
+      }
+   },
+   files: [{
+      attachment: 'https://downloader.disk.yandex.ru/preview/4eb1c62ffc4685f820abea3b7199ee77691f6156b460be776fdb19cfb05624f8/5b5c1f51/_B0aXmp4RJTYYcc2mgnKlmZrPMPqBDJTjveOqRsM_znjQWdJ-s9qohBbDJ6f0E0_nccReKOr0slRp_FHk-xB4g%3D%3D?uid=0&filename=Shak%27Karrukat.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&tknv=v2&size=2048x2048',
+      name: 'Shak'Karrukat.png'
+   }]
+})
+.then(console.log)
+.catch(console.error);
+}
+	
+if(message.content.startsWith(prefix + 'argat')) {
+        message.delete()
+  .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+  .catch(console.error); 
+message.channel.send({
+  embed: {
+    thumbnail: {
+         url: 'attachment://file.png'
+      }
+   },
+   files: [{
+      attachment: 'https://downloader.disk.yandex.ru/preview/d21b01e820b5f05dcec105da217c76da5d872588eaaeb6994cfb532532f3582f/5b5c1fee/_B0aXmp4RJTYYcc2mgnKlqkAn_shecJ8ZSU1qW2472F_heQKq0lGISObLvcpywmGgAyT7xgajA1n7P4dItbklA%3D%3D?uid=0&filename=Argat.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&tknv=v2&size=2048x2048',
+      name: 'Argat.png'
+   }]
+})
+.then(console.log)
+.catch(console.error);
+}
+ 
+if(message.content.startsWith(prefix + 'tilsek')) {
+        message.delete()
+  .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+  .catch(console.error); 
+message.channel.send({
+  embed: {
+    thumbnail: {
+         url: 'attachment://file.png'
+      }
+   },
+   files: [{
+      attachment: 'https://downloader.disk.yandex.ru/preview/da5fdcf9823efdd3e1d29fc0eb5d7f3af37ac4711363f7dfbb3df0119d089c47/5b5c2075/_B0aXmp4RJTYYcc2mgnKlvCKM8VFkUEhPQxg-d0q-6vLaRTNd8styNN0UbuRem1O30SaqvXBmvRC3LBjL0g2VA%3D%3D?uid=0&filename=Tilsek.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&tknv=v2&size=2048x2048',
+      name: 'Tilsek.png'
+   }]
+})
+.then(console.log)
+.catch(console.error);
+}
+
 let messageArray = message.content.split(" ");
 let args = messageArray.slice(1);    
     if(message.content.startsWith(prefix + 'calc')) {
