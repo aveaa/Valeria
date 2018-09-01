9d522ebac057c27eda78118cb52bfc64a18c2554
NordTort <41715818+NorthTort@users.noreply.github.com>
2018-09-01 15:05:59 +0300

    Update Валерия.js

--- Валерия.js
+++ Валерия.js
@@ -35,7 +35,7 @@ var prefix = '!'
 		message.channel.sendMessage('<:KolesoUdachi:469348898058797058> <:KolesoUdachi:469348898058797058> <:KolesoUdachi:469348898058797058> <:KolesoUdachi:469348898058797058> <:KolesoUdachi:469348898058797058> @everyone <:KolesoUdachi:469348898058797058> <:KolesoUdachi:469348898058797058> <:KolesoUdachi:469348898058797058> <:KolesoUdachi:469348898058797058> <:KolesoUdachi:469348898058797058> \n Граждане Имперского города! Делайте ваши ставки! Кто победит в сегодняшнем турнире!? \n Кликайте по иконке претендента!');
 	 }
 	 
-	 if(message.member.roles.has(436925006115766283).content.startsWith(prefix + 'say')) {
+	 if(message.content.startsWith(prefix + 'say')) {
     const args = message.content.slice(prefix.length).split('say'); 
 	  message.delete()
   .then(msg => console.log(`Deleted message from ${msg.author.username}`))
