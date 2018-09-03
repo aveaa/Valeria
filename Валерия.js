24716e7063363bbc149697a58b911216f6165b4b
NordTort <41715818+NorthTort@users.noreply.github.com>
2018-09-04 01:00:40 +0300

    Update Валерия.js

--- Валерия.js
+++ Валерия.js
@@ -62,7 +62,7 @@ message.channel.send({
       }
    },
    files: [{
-      attachment: 'https://downloader.disk.yandex.ru/preview/c52e68fbd3902f8b72e86bd88d7a4072a02fc853742f89c52ba98cb0e6ec98fe/5b8a831d/_B0aXmp4RJTYYcc2mgnKluOcn-5iBOhP-9h0kGRSKv-3NdsWe9y8fZujskxej9v-qu8q6OQpLh7Knynr51DWuw%3D%3D?uid=0&filename=Vinrael.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&tknv=v2&size=2048x2048',
+      attachment: 'https://downloader.disk.yandex.ru/preview/f6edfcacede10841baae988bb03837893e094fe479f20c308140e54ea0ce69e3/5b8de723/_B0aXmp4RJTYYcc2mgnKluOcn-5iBOhP-9h0kGRSKv-3NdsWe9y8fZujskxej9v-qu8q6OQpLh7Knynr51DWuw%3D%3D?uid=0&filename=Vinrael.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&tknv=v2&size=2048x2048',
       name: 'Win hero.png'
    }]
 })
